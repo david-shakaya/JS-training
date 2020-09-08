453a78b8f49ba1406caf4d0fd02017ae5b84ba04
david-shakaya <devidoz666@gmail.com>
2020-09-08 19:58:58 +0300

    +

---
+++ training.js
@@ -0,0 +1,18 @@
+// const mesage = "hello world";
+// console.log("Жоаень---", mesage);
+
+// const yourName = confirm("Укажите пожалуйста ваше имя");
+// console.log(yourName);
+
+// const city = prompt("Уrажите ваш город");
+// const height = prompt("Какая длина члена");
+// console.log(height, city);
+
+// Проверка на число
+// console.log(isNaN(name));
+
+const name = '«Генератор защитного поля»,';
+const price = '1000';
+console.log('Выбран', name, 'цена за штуку', Number(price), 'кредитов.');
+
+console.log('Выбран', name, 'цена за штуку', price * 2, 'кредитов.');
