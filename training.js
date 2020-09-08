eb91f031f74618415408e35426d351e8fefe5d92
david-shakaya <devidoz666@gmail.com>
2020-09-08 22:13:47 +0300

    +

--- training.js
+++ training.js
@@ -11,8 +11,31 @@
 // Проверка на число
 // console.log(isNaN(name));
 
+// ПРОВЕРКА НА ТИП
+// const type = typeof price;
+// console.log(type);
+// ЛИБО
+// console.log(typeof name);     ---name это имя(идентификатор)
+
+/*confirm - Вопрос к юзеру(либо ок либо cancel)
+ const yourName = confirm("Укажите пожалуйста ваше имя");
+console.log(yourName);
+
+
+*prompt - ВОПРОС К ЮЗЕРУ с полем для ответа 
+ const city = prompt("Уrажите ваш город");
+// const height = prompt("Какая длина члена");
+// console.log(height, city);
+
+*/
+
+// ПРИСВОЕНИЕ ТИПА  Number
+// let age = prompt('Введите ваш возраст');
+// age = Number(age);
+// console.log('Возраст посетителя:', age, 'лет');
+
 const name = '«Генератор защитного поля»,';
-const price = '1000';
-console.log('Выбран', name, 'цена за штуку', Number(price), 'кредитов.');
+const price = 1000;
+console.log('Выбран', name, 'цена за штуку', price, 'кредитов.');
 
 console.log('Выбран', name, 'цена за штуку', price * 2, 'кредитов.');
