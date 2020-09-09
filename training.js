8797d1ae35ee83b0a3b4eb4d66fb3706168b859a
david-shakaya <devidoz666@gmail.com>
2020-09-09 11:40:17 +0300

    +

--- training.js
+++ training.js
@@ -34,13 +34,32 @@ console.log(yourName);
 // age = Number(age);
 // console.log('Возраст посетителя:', age, 'лет');
 
-// Значение parseInt(показывает число без букв 50px)   и  parseFloat(показывает числа с дробью 1.221)
-// let height = '29px';
-// height = Number.parseInt(height);
-// console.log('высота:', height, 'px');
+/* 
+Значение parseInt(показывает число без букв 50px)   и  parseFloat(показывает числа с дробью 1.221)
+let height = '29px';
+height = Number.parseInt(height);
+console.log('высота:', height, 'px'); */
 
+/* 
+АНАЛОГ КОНКАТИНАЦИИ СШИВКИ СЛОВ (ДОВОЛЬНО УДОБНО)
+const sameMessage = `My name is ${name}, I'm ${age} years old and ${mood}.`;
+console.log(sameMessage)
+ */
+
+/* МЕТОД ЧИСЛО  .toFixed (Отсечение цыфр после точки) */
+
+// let price = 1000.12334;     /* <--Переменная */
+// price = price.toFixed(2);   /* <-- Оставляет количество цифр указанных в скобках(2) после точки */
+// price = Number(price);      /* <-- Присваевает тип числа "Number" */
+// console.log(price);        /* <-- Выводит в консоль */
+
+/* Тоже самое что и выше только написано короче(метод матрешки)
+let price = 1000.12334;
+price = Number(price.toFixed(2));
+console.log(price);
+ */
 const name = '«Генератор защитного поля»,';
-const price = 1000;
+const price = 1000.12334;
 console.log('Выбран', name, 'цена за штуку', price, 'кредитов.');
 
 console.log('Выбран', name, 'цена за штуку', price * 2, 'кредитов.');
